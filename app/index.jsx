e049bbf61bda1622cc385c94e539cfae2276ff52
WhiteCat2023 <berndtdenniscanaya@gmail.com>
2025-07-19 23:09:43 +0800

    chore: add nativewind

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import "../global.css"
 
 export default function Index() {
   return (
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Ariba</Text>
+      <Text className="text-xl font-bold text-blue-500">Ariba</Text>
     </View>
   );
 }
